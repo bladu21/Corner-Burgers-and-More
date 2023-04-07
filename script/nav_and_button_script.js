224ee349e7b55d0037ef33ee87bbf9f78f54ca6e
bladu21 <124273941+bladu21@users.noreply.github.com>
2023-04-07 18:14:12 +0300

    Update nav_and_button_script.js

--- script/nav_and_button_script.js
+++ script/nav_and_button_script.js
@@ -1,14 +1,13 @@
 window.onload = function() {
     
     function hamMenu() {
-        
-        const navMenu = document.querySelector(".nav__menu");
-      
         hamburger.classList.toggle("active");
         navMenu.classList.toggle("active");
       };
       
       const hamburger = document.querySelector(".hamburger");
+      const navMenu = document.querySelector(".nav__menu");
+
       hamburger.addEventListener("click", hamMenu);
 
     };
