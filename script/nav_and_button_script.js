80aaed908d88e36fa44c5048760ac9053ca63c83
bladu21 <124273941+bladu21@users.noreply.github.com>
2023-08-16 23:36:12 +0300

    accordion__menu__page

--- script/nav_and_button_script.js
+++ script/nav_and_button_script.js
@@ -1,23 +1,35 @@
 window.onload = function() {
     
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav__menu");
+
     function hamMenu() {
         hamburger.classList.toggle("active");
         navMenu.classList.toggle("active");
-      };
-      
-      const hamburger = document.querySelector(".hamburger");
-      const navMenu = document.querySelector(".nav__menu");
-
-      hamburger.addEventListener("click", hamMenu);
-
     };
+    hamburger.addEventListener("click", hamMenu);
 
-    function openMenu() {
-        window.location.href = "html/Corner_burger_MenuPage.html";
-    };
+    const acc = document.getElementsByClassName("accordion");
     
-    
-    function myForm() {
-        alert("Съобщението Ви е изпратено успешно!");
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            
+            let panel = this.nextElementSibling;
+            
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
     };
-  
+};
+
+function openMenu() {
+    window.location.href = "html/Corner_burger_MenuPage.html";
+};
+
+function myForm() {
+    alert("Съобщението Ви е изпратено успешно!");
+};
